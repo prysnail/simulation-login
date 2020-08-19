9b6a47ddecfc1b13930c8dfb2f20cec1a16741c1
徐红兵 <xuhongbing11@crc.com.hk>
2020-08-19 21:36:12 +0800

    项目初始哈

---
+++ src/main/java/com/prysnail/simulation/login/SimulationLoginApplication.java
@@ -0,0 +1,13 @@
+package com.prysnail.simulation.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulationLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimulationLoginApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/prysnail/simulation/login/SimulationLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prysnail.simulation.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimulationLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
